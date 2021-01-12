49952a5aef55e0192fd8c9ab675a1243a6ecd208
Matt <mpickle86@gmail.com>
2021-01-11 18:52:41 -0600

    Create snake

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const grid = document.querySelector(".grid");
 const startButton = document.querySelector("#start");
 const score = document.querySelector("#score");
 let squares = [];
+let currentSnake = [0,1,2];
 
 function createGrid() {
   for (i = 0; i < 100; i++) {
@@ -13,3 +14,5 @@ function createGrid() {
 }
 
 createGrid();
+
+currentSnake.forEach(index => squares[index].classList.add("snake"));;
