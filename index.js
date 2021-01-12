7c863441e4d930191eec4751ed8567898d444a2f
Matt <mpickle86@gmail.com>
2021-01-11 18:39:03 -0600

    Create grid squares in javascript

---
+++ index.js
@@ -0,0 +1,15 @@
+const grid = document.querySelector(".grid");
+const startButton = document.querySelector("#start");
+const score = document.querySelector("#score");
+let squares = [];
+
+function createGrid() {
+  for (i = 0; i < 100; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    grid.appendChild(square);
+    squares.push(square);
+  }
+}
+
+createGrid();
